e9b53dfe1336d1ace7981e0d8d71d3204e674b4d
Chinaemere Ike <ms.chinaemere@gmail.com>
2019-11-12 03:46:55 +0000

    Updated.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import Header from "./components/Header.js";
+import React from 'react';
+import Header from './components/Header.js';
+import AppRouter from './components/Router.js';
+import CharacterCard from './components/CharacterCard.js';
 
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+  return <main>
+    <Header />
+    <AppRouter />
+    <CharacterCard />
+
+  </main>
 }
